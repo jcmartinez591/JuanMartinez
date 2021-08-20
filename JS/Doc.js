41362f6d1debe67110233dc3596f4413cc65e5f5
jcmartinez591 <jcmartinez591@gmail.com>
2021-08-20 11:47:00 -0500

    JavaScript

--- JS/Doc.js
+++ JS/Doc.js
@@ -4,4 +4,14 @@ const producto=
     precio:300,
     disponible:true
 }
-console.log(producto);
+
+const {nombre}=producto;
+
+console.log(nombre);
+
+///Destructuring con arreglos
+
+const numeros =[10,20,30,40,50,60];
+
+const [primero]=numeros;
+console.log(primero);
