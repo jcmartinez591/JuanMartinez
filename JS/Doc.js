f5640b99c82a304a6cc81a4c22e42be3410adbe0
jcmartinez591 <jcmartinez591@gmail.com>
2021-08-20 11:58:34 -0500

    JS1

--- JS/Doc.js
+++ JS/Doc.js
@@ -13,5 +13,20 @@ console.log(nombre);
 
 const numeros =[10,20,30,40,50,60];
 
-const [primero]=numeros;
+const [...tercero]=numeros;
 console.log(primero);
+
+
+//Metodos de Arreglos
+
+const Carrito =
+[
+    {nombre1:'Monitor 27 Pulgadas', Precio1: 500},
+    {nombre1:'Televisión', Precio1:100},
+    {nombre1:'Tablet', Precio1: 200},
+]
+
+for (let i = 0; i <Carrito.length; I++)
+{
+    console.log(`${Carrito[i].nombre1}-Precio ${Carrito[1].Precio1}`);
+}
